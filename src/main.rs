a68b1739cdf5f31b0ac4d902c97049525afcf855
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-11-28 03:40:44 +0900

    Use test.html to test

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ entry_point!(main);
 
 fn main() -> u64 {
     let client = HttpClient::new();
-    match client.get("example.com".into(), 80, "/".into()) {
+    match client.get("host.test".into(), 8000, "/test.html".into()) {
         Ok(res) => print!("response:\n{res:#?}"),
         Err(e) => print!("error:\n{e:#?}"),
     }
