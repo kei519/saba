ff3fc85cd838b0b26f2295bc78d810900d6ac5d6
kei519 <masaki.keigo.q00@kyoto-u.jp>
2025-01-15 22:57:19 +0900

    main: Browser を動作させてみて情報を表示

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 #![no_std]
 #![cfg_attr(not(target_os = "linux"), no_main)]
 
-use net_wasabi::http::HttpClient;
-use noli::prelude::*;
+extern crate alloc;
 
-entry_point!(main);
+use alloc::string::ToString;
+
+use noli::prelude::*;
+use saba_core::{browser::Browser, http::HttpResponse};
+
+static TEST_HTTP_RESPONSE: &str = r#"HTTP/1.1 200 OK
+Data: xx xx xx
+
+
+
+<html>
+<head></head>
+<body>
+  <h1 id="title">H1 title</h1>
+  <h2 class="class">H2 title</h2>
+  <p>Test text.</p>
+  <p>
+    <a href="example.com">Link1</a>
+    <a href="example.com">Link2</a>
+  </p>
+</body>
+</html>
+"#;
 
 fn main() -> u64 {
-    let client = HttpClient::new();
-    match client.get("host.test".into(), 8000, "/test.html".into()) {
-        Ok(res) => print!("response:\n{res:#?}"),
-        Err(e) => print!("error:\n{e:#?}"),
+    let browser = Browser::new();
+
+    let response =
+        HttpResponse::new(TEST_HTTP_RESPONSE.to_string()).expect("failed to parse http response");
+    let page = browser.borrow().current_page();
+    let dom_string = page.borrow_mut().receive_response(response);
+
+    for log in dom_string.lines() {
+        println!("{}", log);
     }
+
     0
 }
+
+entry_point!(main);
